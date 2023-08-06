94cbc8470c04e3bf6618210ba6c24dbc373411ff
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-06 21:51:52 +0100

    sqlalchemy model

--- models.py
+++ models.py
@@ -1,2 +1,15 @@
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, ARRAY
 from sqlalchemy.orm import declarative_base
+from uuid import UUID, uuid4
+from typing import List
+
+Base = declarative_base()
+
+
+class Book(Base):
+    __tablename__ = "books"
+
+    id = Column(Integer, primary_key=True)
+    title = Column(ARRAY(String))
+    number = Column(Integer)
+    locccn = Column(Integer)
