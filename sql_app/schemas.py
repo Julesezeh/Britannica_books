3388111149deff5c76e6330ef15f34b894600827
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-07 14:16:10 +0100

    schema with pydantic

---
+++ sql_app/schemas.py
@@ -0,0 +1,35 @@
+from pydantic import BaseModel
+
+
+class BaseBook(BaseModel):
+    title: list[str] = []
+    description: str | None = None
+
+
+class BookCreate(BaseBook):
+    pass
+
+
+class Book(BaseBook):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class UserBase(BaseModel):
+    email: str
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class User(UserBase):
+    id: int
+    is_active: bool
+    items: list[Book] = []
+
+    class Config:
+        orm_mode = True
