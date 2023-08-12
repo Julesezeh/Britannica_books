83475ec6b198b65c44166b3105830d4206ea3ff2
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-12 21:29:01 +0100

    environent variables for db credentials

--- sql_app/database.py
+++ sql_app/database.py
@@ -1,10 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+import loadenv
+
+loadenv()
+username = os.environ.get("USERNAME")
+password = os.environ.get("PASSWORD")
+host = os.environ.get("HOST")
+database = os.environ.get("DATABASE")
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://freedb_jules:TVz9esH#Sgz4yV&@sql.freedb.tech:3306/freedb_brittanica_books"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://{username}:{password}@{host}:3306/{database}"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
