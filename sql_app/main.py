3f5b1b3a32b6f2d31f0779564fc5853bdce93e78
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-12 14:59:49 +0100

    paths appropriately named

--- sql_app/main.py
+++ sql_app/main.py
@@ -18,7 +18,7 @@ def get_db():
         db.close()
 
 
-@app.post("/users/")
+@app.post("/api/v1/users/")
 async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
@@ -26,13 +26,13 @@ async def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
 
-@app.get("/users/")
+@app.get("/api/v1/users/")
 async def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
 
-@app.get("/users/{user_id}")
+@app.get("/api/v1/users/{user_id}")
 async def read_user(user_id: int, db: Session = Depends(get_db)):
     db_user = crud.get_user(db, user_id=user_id)
     if db_user is None:
@@ -40,26 +40,26 @@ async def read_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
-@app.post("/users/{user_id}/books/")
+@app.post("/api/v1/users/{user_id}/books/")
 async def create_book_for_user(
     user_id: int, book: schemas.BookCreate, db: Session = Depends(get_db)
 ):
     return crud.create_book(db=db, book=book, user_id=user_id)
 
 
-@app.get("/books/")
+@app.get("/api/v1/books/")
 async def read_books(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     books = crud.get_books(db, skip=skip, limit=limit)
     return books
 
 
-@app.get("/book/")
+@app.get("/api/v1/book/")
 async def get_book_by_locccn(locccn: int, db: Session = Depends(get_db)):
     book = crud.get_book_by_locccn(db, locccn=locccn)
     return book
 
 
-@app.put("/books/{id}")
+@app.put("/api/v1/books/{id}")
 async def update_book(id: int, book: schemas.UpdateBook, db: Session = Depends(get_db)):
     book = crud.update_book(db, id, book)
     return book
