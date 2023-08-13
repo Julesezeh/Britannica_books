b5a60c45549960e21d416d6fe4cda9b1cbaedd02
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-13 05:50:43 +0100

    environment variable names changed

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,20 +2,22 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
-import loadenv
+from dotenv import load_dotenv
 
-loadenv()
-username = os.environ.get("USERNAME")
-password = os.environ.get("PASSWORD")
-host = os.environ.get("HOST")
-database = os.environ.get("DATABASE")
+load_dotenv()
+username = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+database = os.getenv("DB_DATABASE")
 
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 SQLALCHEMY_DATABASE_URL = (
     f"mysql+pymysql://{username}:{password}@{host}:3306/{database}"
 )
-
+print(username)
+print(password)
+print(SQLALCHEMY_DATABASE_URL)
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
