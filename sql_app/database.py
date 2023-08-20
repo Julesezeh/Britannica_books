913d657215f75718ed89148783e2e5e2fbd2354e
Jules Okoye-ezeh <julesezeh23@gmail.com>
2023-08-20 12:19:45 +0100

    switched back to sqlite database

--- sql_app/database.py
+++ sql_app/database.py
@@ -5,18 +5,20 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-username = os.getenv("DB_USERNAME")
-password = os.getenv("DB_PASSWORD")
-host = os.getenv("DB_HOST")
-database = os.getenv("DB_DATABASE")
+# username = os.getenv("DB_USERNAME")
+# password = os.getenv("DB_PASSWORD")
+# host = os.getenv("DB_HOST")
+# database = os.getenv("DB_DATABASE")
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./database.db"
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = (
-    f"mysql+pymysql://{username}:{password}@{host}:3306/{database}"
-)
+# SQLALCHEMY_DATABASE_URL = (
+#     f"mysql+pymysql://{username}:{password}@{host}:3306/{database}"
+# )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
